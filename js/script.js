db8f518e075929907360a04eb0754b121e693c90
AstR0x <vesel4ack98@mail.ru>
2018-10-21 00:29:40 +0300

    21:10:18

--- js/script.js
+++ js/script.js
@@ -1,8 +1,47 @@
-document.onload(function() {
-    $('#7').click(function () {
-        $('.display_input').value = '7';
-    });
-});
+$(document).ready(function() {
+    var display = $('.display__input');
+    var key = $('.button-block button');
+    var button = $('.button-block__button')
+    var clear = $('.button-block__clear');
+    var equal = $('.button-block__equal');
+    var operations = $('.operations-block__button');
 
-document.getElementsByClassName('button-block').innerHTML = "dadada";
-document.innerHTML = 'da';
+    key.each(function() {
+        var current = $(this).attr('value');
+        $(this).text(current);
+    })
+
+    button.click(function() {
+        display.val(display.val() + $(this).val());
+    })
+
+    clear.click(function() {
+        display.val('');
+    })
+
+    equal.click(function() {
+        display.val(eval(display.val()));
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
