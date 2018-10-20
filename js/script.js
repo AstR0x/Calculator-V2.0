2dc026d24240ff6d3fac2f1550f525a79c48a5eb
AstR0x <vesel4ack98@mail.ru>
2018-10-20 21:14:27 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.onload(function() {
+    $('#7').click(function () {
+        $('.display_input').value = '7';
+    });
+});
+
+document.getElementsByClassName('button-block').innerHTML = "dadada";
+document.innerHTML = 'da';
